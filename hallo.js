138d6dd7c8d93cb2ca8b3acdc59ba3c54fbfad8d
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2018-09-24 12:13:45 +0200

    Initial commit

---
+++ hallo.js
@@ -0,0 +1,16 @@
+function hello(){
+    x = 0;
+    y = 0;
+    z = 0;
+
+    for(x=0; x<=10; x++) {
+        for(y=0; y<=10; y++) {
+            z = x+y +10;
+        }
+    }
+    console.log(z);
+    return z;
+}
+
+
+
